050a34f04ae8971a49b00df0e4ee956136e64fef
najm-choueiry <nachoueiry@outlook.com>
2023-11-04 08:47:48 +0200

    <>Creating the fetch API

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "554c73a57emshe03124f2d676351p1edce4jsn4aa7d0fb189a",
-    "X-RapidAPI-Host": "",
-  },
-};
-
-fetch("https://shazam-core.p.rapidapi.com/v1/charts/genre-world", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
-
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    prepareHeaders: (headers) => {
+      headers.set(
+        "X-RapidAPI-Key",
+        " 554c73a57emshe03124f2d676351p1edce4jsn4aa7d0fb189a"
+      );
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: "/charts/world" }),
+  }),
 });
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
