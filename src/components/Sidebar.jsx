5b91244a94756755aaed8ff4cf532a0b877e3887
najm-choueiry <nachoueiry@outlook.com>
2023-11-27 19:44:18 +0200

    <> Creating the navbar for desktop users

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,24 @@ import { RiCloseLine } from "react-icons/ri";
 import { logo } from "../assets";
 import { links } from "../assets/constants";
 
+const NavLinks = ({ handleClick }) => {
+  return (
+    <div className="mt-10">
+      {links.map((item) => (
+        <NavLink
+          className="flex flex-row justify-start items-center my-8 text-sm font-medium text-gray-400 hover:text-cyan-400"
+          key={item.name}
+          to={item.to}
+          onClick={() => handleClick && handleClick()}
+        >
+          <item.icon className="w-6 h-6 mr-2" />
+          {item.name}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
+
 const Sidebar = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
@@ -11,7 +29,10 @@ const Sidebar = () => {
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-[#191624]">
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
       </div>
+
+      <div className="absolute md:hidden block top-6 right-3"> </div>
     </>
   );
 };
