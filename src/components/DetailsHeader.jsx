cb9598d72ddde5e2b593a7223e044a3780f3db81
najm-choueiry <nachoueiry@outlook.com>
2023-12-12 09:21:08 +0200

    <> Adjusting the lyrics for the song

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 
 const DetailsHeader = ({ artistId, artistData, songData }) => {
+  const artist = artistData?.artists[artistId]?.attributes;
   return (
     <div className="relative w-full flex flex-col">
       <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
@@ -10,16 +11,26 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
           alt="Art"
           src={
             artistId
-              ? artistData?.artists[artistId].attributes?.artwork?.url
-                  .replace("{w}", "500")
-                  .replace("{h}", "500")
+              ? artist.artwork?.url.replace("{w}", "500").replace("{h}", "500")
               : songData?.images?.coverart
           }
           className="sm:w-48 w-28 sm:h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
         />
 
         <div className="ml-5">
-          <p> {artistId?.artistData?.artists[artistId].attributes.name} </p>
+          <p className="font-bold sm:text-3xl text-xl">
+            {artistId ? artist?.name : songData?.title}{" "}
+          </p>
+          {!artistId && (
+            <Link to={`/artists/${songData?.artists[0].adamid}`}>
+              <p className="text-base text-gray-400 mt-2">
+                {songData?.subtitle}
+              </p>
+            </Link>
+          )}
+          <p className="text-base text-gray-400 mt-2">
+            {artistId ? artist?.genreNames[0] : songData?.genres?.primary}
+          </p>
         </div>
       </div>
     </div>
