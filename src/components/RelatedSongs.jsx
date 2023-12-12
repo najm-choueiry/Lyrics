6c1f27f86feb80a39f53d863bfb5b97eceb2d024
najm-choueiry <nachoueiry@outlook.com>
2023-12-12 12:26:17 +0200

    <> Implementing the related songs

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,3 +1,31 @@
-const RelatedSongs = () => <div>Loader</div>;
+import { MdAddToHomeScreen } from "react-icons/md";
+import SongBar from "./SongBar";
+
+const RelatedSongs = ({
+  data,
+  isPlaying,
+  handlePauseClick,
+  handlePlayClick,
+  artistId,
+}) => {
+  return (
+    <div className="flex flex-col">
+      <h1 className="font-bold text-3xl text-white"> Related Songs</h1>
+      <div className="mt-6 w-full flex flex-col">
+        {data?.map((song, i) => (
+          <SongBar
+            key={`${song.key}-${artistId}`}
+            song={song}
+            artistId={artistId}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            handlePauseClick={handlePauseClick}
+            handlePlayClick={handlePlayClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default RelatedSongs;
